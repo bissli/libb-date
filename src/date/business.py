a82dc0c0edd4753a0e9bf3dad62321617ec85aeb
bissli <bissli@xyz.com>
2024-05-22 12:06:36 +0000

    add is_business_day util

--- src/date/business.py
+++ src/date/business.py
@@ -1,26 +1,28 @@
 from date import NYSE, DateTime, Entity
 
-__all__ = ['within_business_hours']
+__all__ = ['is_within_business_hours', 'is_business_day']
 
 
-def within_business_hours(entity: Entity = NYSE):
-    """
+def is_within_business_hours(entity: Entity = NYSE) -> bool:
+    """Return whether the current native datetime is between
+    open and close of business hours.
+
     >>> from unittest.mock import patch
     >>> tz = NYSE.tz
 
     >>> with patch('date.DateTime.now') as mock:
     ...     mock.return_value = DateTime(2000, 5, 1, 12, 30, 0, 0, tzinfo=tz)
-    ...     within_business_hours()
+    ...     is_within_business_hours()
     True
 
     >>> with patch('date.DateTime.now') as mock:
     ...     mock.return_value = DateTime(2000, 7, 2, 12, 15, 0, 0, tzinfo=tz) # Sunday
-    ...     within_business_hours()
+    ...     is_within_business_hours()
     False
 
     >>> with patch('date.DateTime.now') as mock:
     ...     mock.return_value = DateTime(2000, 11, 1, 1, 15, 0, 0, tzinfo=tz)
-    ...     within_business_hours()
+    ...     is_within_business_hours()
     False
 
     """
@@ -28,6 +30,12 @@ def within_business_hours(entity: Entity = NYSE):
     this_entity = DateTime(entity=entity, tzinfo=entity.tz).now()
     bounds = this_entity.business_hours()
     return this_entity.business_open() and (bounds[0] <= this.astimezone(entity.tz) <= bounds[1])
+
+
+def is_business_day(entity: Entity = NYSE) -> bool:
+    """Return whether the current native datetime is a business day.
+    """
+    return DateTime.now().astimezone(entity.tz).is_business_day()
 
 
 if __name__ == '__main__':
